5ca369fac71d72b8e2a5391810f7c6c0426b2402
FJOden <123657902+FJOden@users.noreply.github.com>
2025-01-27 12:05:27 -0600

    awesome

---
+++ 3Learns.py
@@ -0,0 +1,3 @@
+#I would like to learn how to execute an Omaplata
+#I would like to learn how to Buy a House
+#I would like to learn how to develop a stock trading strategy that can be automated and backtested for accuracy.
